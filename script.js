36c842c51b3bfe6b4edc538622943b843ff3897f
pyreczka <m.spyra1989@gmail.com>
2026-09-19 20:58:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ document.querySelector('.ladny-przycisk').addEventListener('click', function () 
     alert('No Widzisz Nic Się Nie Stało');
 });
 
-document.querySelector('github').addEventListener('click', function () {
-    window.location.href = 'https://github.com/pyreczka/';
-});
-
 document.querySelector('.invisible-btn').addEventListener('click', function () {
     window.location.href = 'https://www.youtube.com/watch?v=nso6Vhg0p9k';
 });
