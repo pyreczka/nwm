a3c4c68e509647f3214012e05efbba629a462f19
pyreczka <m.spyra1989@gmail.com>
2026-09-19 20:51:40 +0200

    Add GitHub link button functionality

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ document.querySelector('.ladny-przycisk').addEventListener('click', function () 
     alert('No Widzisz Nic Się Nie Stało');
 });
 
+document.querySelector('.github').addEventListener('click', function () {
+    window.location.href = 'https://github.com/pyreczka/';
+});
+
 document.querySelector('.invisible-btn').addEventListener('click', function () {
     window.location.href = 'https://www.youtube.com/watch?v=nso6Vhg0p9k';
 });
